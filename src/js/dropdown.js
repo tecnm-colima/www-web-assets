c75d0a253860e6754b4624e2131392fe5b9eaeb8
Marco E <marco_eleno1@hotmail.com>
2022-12-06 11:37:40 -0600

    ADD dropdown-v2.0 change TXT,add list

---
+++ src/js/dropdown.js
@@ -0,0 +1,36 @@
+$(document).ready(() =>{
+    $('.dropdown--responsive').hide();
+    responsivo()
+    $(window).resize(() =>{
+        responsivo()
+    }) 
+    $('.dropdown--responsive').click(() => {
+        $('.dropdown-content').toggleClass('show')
+    })
+    if ($(window).width() < 1200) {
+
+        $('.dropdown-item').click(function(e) {
+            e.stopPropagation();
+            if(this.lastElementChild.tagName == 'UL'){
+                $(this.lastElementChild).toggleClass('show')
+                if($(this).hasClass('activo')){
+                    $(this).find('.show').removeClass('show');
+                    $(this).find('.activo').removeClass('activo');
+                }
+                $(this).toggleClass('activo')
+            }
+        }) 
+    }   
+})
+function responsivo(){
+    $('li .show').removeClass('show')
+    $('.activo').removeClass('activo')
+    if ($(window).width() < 1200) {
+        $('.dropdown-content').hide();
+        $('.dropdown--responsive').show();
+    }else{
+        $('.dropdown-content').show();
+        $('.dropdown-content').attr('display','flex !important');
+        $('.dropdown--responsive').hide();
+    }
+}
